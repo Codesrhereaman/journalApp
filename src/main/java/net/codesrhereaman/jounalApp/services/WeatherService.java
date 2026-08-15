8868e9ea3445478102806306ff42d65a463f0620
Aman <amanbhardwaj202131@gmail.com>
2026-08-15 14:47:43 +0530

    adding weather api

--- src/main/java/net/codesrhereaman/jounalApp/services/WeatherService.java
+++ src/main/java/net/codesrhereaman/jounalApp/services/WeatherService.java
@@ -1,23 +1,26 @@
 package net.codesrhereaman.jounalApp.services;
 
+import lombok.RequiredArgsConstructor;
 import net.codesrhereaman.jounalApp.JournalEntry.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class WeatherService {
 
 
+    private final RestTemplate restTemplate;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @Value("${weather.api.key}")
+    private  String apiKey ;
 
-    private final String apiKey = "4bffe0f2ce050d167aa6eb474a1e2065";
-
-    private String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=City";
+    private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=City";
 
     public  WeatherResponse weatherResponse(String city){
         String finalApi = API.replace("API_KEY",apiKey).replace("City",city);
